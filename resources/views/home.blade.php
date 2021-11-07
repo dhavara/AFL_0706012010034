f2b7d4fceae19e945792eec9a8ae2c7a70db26b4
dhavara <dhava.rachmana.aziz@gmail.com>
2021-11-07 09:46:53 +0700

    added button in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,20 @@
 @extends('layout.mainlayout')
 
-@section('title','Home')
+@section('title', 'Home')
 
 @section('page_title')
-<h1 class="home">{{ $pagetitle }}</h1>
+    <h1 class="home">{{ $pagetitle }}</h1>
 @endsection
 
 @section('main_content')
     <h5 class="home">This is a homepage</h5>
+    <h5 class="home">You can add Mahasiswa or Prodi below</h5>
+    <div class="container">
+        <a class="home" href="{{ route('mahasiswa.create') }}">
+            <button type="submit" class="btn btn-success mb-2"> + Add Mahasiswa</button>
+        </a>
+        <a class="home" href="{{ route('prodi.create') }}">
+            <button type="submit" class="btn btn-success mb-2"> + Add Prodi</button>
+        </a>
+    </div>
 @endsection
